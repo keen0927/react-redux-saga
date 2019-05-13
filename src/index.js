82b99aaa6b2b3f6f24b2c162c050b4d197abcc50
KEEN <kyh0927@live.co.kr>
2019-05-13 23:27:57 +0900

    190513

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import App from './App';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from'redux-saga';
+import { watchAgeUp } from "./sagas/saga";
 
-const store = createStore(reducer);
+const sagaMiddlewhare = createSagaMiddleware();
+
+
+
+const store = createStore(reducer, applyMiddleware(sagaMiddlewhare));
+sagaMiddlewhare.run(watchAgeUp);
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
